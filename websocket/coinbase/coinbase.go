ffd3b6b4fd01b1019f97994c43c3098092dc62a0
Ignatius Teo <iggy@codeworx.com.au>
2022-03-04 14:38:16 +1000

    Fold into client

--- websocket/coinbase/coinbase.go
+++
@@ -1,8 +0,0 @@
-package coinbase
-
-const (
-	// ProdWSURL is the production websocket URL.
-	ProdWSURL = "wss://ws-feed.exchange.coinbase.com"
-	// SandboxWSURL is the sandbox websocket URL.
-	SandboxWSURL = "wss://ws-feed-public.sandbox.exchange.coinbase.com"
-)
